f2e081e7f57fbb3e692950004682d8226987df4a
Mariusz Borowiak <neutron.meme@proton.me>
2023-11-28 21:27:21 +0000

    Model setup

---
+++ WebAPI-OData/WebAPI/Models/DatabaseContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WebAPI.Models
+{
+    public class DatabaseContext : DbContext
+    {
+        public DatabaseContext(DbContextOptions<DatabaseContext>options) : base(options)
+        {
+        }
+
+        public DbSet<Stockboards_master> Stockboards { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        } 
+    }
+}
